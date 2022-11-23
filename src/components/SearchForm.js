872fa09fc156e46c6fee169162a9c5a1083e4847
IdanKfir <zunzer244@gmail.com>
2022-11-23 19:30:34 +0200

    minorChanges

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -19,7 +19,7 @@ const SearchForm = () => {
     <section className='section search'>
       <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
-          <label htmlFor='name'>search your favorit cocktail</label>
+          <label htmlFor='name'>search your favorite cocktail</label>
           <input
             type='text'
             id='name'
