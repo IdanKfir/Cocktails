fcb363b882179cd891977d715ffdc367a9fed7af
IdanKfir <zunzer244@gmail.com>
2022-11-23 19:23:50 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        sequi quod. Dicta accusamus ipsa aut eius quos nihil nam sint suscipit!
+        Repellat tenetur soluta minima, ratione, in sapiente quam asperiores
+        necessitatibus expedita illum quibusdam eveniet molestias, facere iusto
+        quaerat consequuntur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
